9a6c88347987b86f47995d006512ce8a35947a6a
hakambing <abdulhakamhasbullah@gmail.com>
2023-03-05 09:20:16 +0800

    finished the styling

---
+++ script.js
@@ -0,0 +1,13 @@
+zero = document.querySelector("#zeroBtn");
+const one = document.getElementById("oneBtn");
+const two = document.getElementById("twoBtn");
+const three = document.getElementById("threeBtn");
+const four = document.getElementById("fourBtn");
+const five = document.getElementById("fiveBtn");
+const six = document.getElementById("sixBtn");
+const seven = document.getElementById("sevenBtn");
+const eight = document.getElementById("eightBtn");
+const nine = document.getElementById("nineBtn");
+numbers = document.getElementById("numbers");
+
+zero.onclick = () => numbers.innerHTML = "0";
